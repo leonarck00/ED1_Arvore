7069e21b006c679bea834a0318b6422fbb74ac58
leonarck00 <40524700+leonarck00@users.noreply.github.com>
2018-11-07 08:04:21 -0200

    Add files via upload

--- arvore/abp.cpp
+++ arvore/abp.cpp
@@ -2,5 +2,31 @@
 
 ABP::ABP()
 {
-
+    nulo = no::montano(0);
+    raiz = nulo;
 }
+
+bool ABP::inserir(item* aux){
+    item*
+}
+
+
+item* ABP::consultar(item* aux){
+    if(aux == 0) return 0;
+    no* ptr = raiz;
+
+    while(ptr != nulo){
+        if(aux -> getcodBarras() < ptr -> getDados()->getcodBarras()){
+            ptr = ptr->getfilhoEsquerdo();
+        }else
+            if(aux->getcodBarras() > ptr->getDados()->getcodBarras()){
+                ptr = ptr->getfilhoDireito();
+            }else{
+                break;
+            }
+    }
+}
+if(ptr == nullo) return 0;
+item* copia = new item;
+*copia = *ptr->getDados();
+return copia;
--- arvore/abp.h
+++ arvore/abp.h
@@ -1,25 +1,29 @@
 #ifndef ABP_H
 #define ABP_H
 #include "no.h"
+#include <iostream>
 
 
 class ABP
 {
 private:
-    no* raiz;
+    no* raiz, *nulo;
 public:
     ABP();
-    logico inserir(item*);
+    bool inserir(item*);
     item* retirar(item*);
     item* consultar(item*);
-    std::string mostrarOrdem();
-    std::string mostrarPosOrdem();
-    std::string mostrarPreOrdem();
-    logico vazia();
-    no* minimo(no*);
-    no* maximo(no*);
-    no* antecessor(no*);
-    no* sucessor(no*);
+    ~ABP();
+private:
+    no* minimo(no*)const;
+    no* maximo(no*)const;
+    no* antecessor(no*)const;
+    no* sucessor(no*)const;
+public:
+    bool vazia()const;
+    std::string mostrarOrdem()const;
+    std::string mostrarPosOrdem()const;
+    std::string mostrarPreOrdem()const;
 };
 
 #endif // ABP_H
